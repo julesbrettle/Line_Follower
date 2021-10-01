2b47eea9fba304672891c94a8f1d7753c2d83065
Julia (Jules) Brettle <57976312+julesbrettle@users.noreply.github.com>
2021-10-01 11:27:29 -0400

    Update line_follower.ino

--- line_follower/line_follower.ino
+++ line_follower/line_follower.ino
@@ -1,11 +1,23 @@
+#include <Wire.h>
 #include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *motorL = AFMS.getMotor(1);
+Adafruit_DCMotor *motorR = AFMS.getMotor(2);
 
 void setup() {
   // put your setup code here, to run once:
+  AFMS.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  motorL->setSpeed(150);
+  motorL->run(FORWARD);
+
+  
 
 }
